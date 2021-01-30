14049d571b94e3a11a4d83cdb9a55bfcf56acad3
HS4QWC <77999810+hs4qwc@users.noreply.github.com>
2021-01-30 18:39:48 +0700

    Add files via upload

---
+++ LCD-basic.ino
@@ -0,0 +1,37 @@
+/*
+ The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+// include the library code:
+#include <LiquidCrystal.h>
+const int rs = 12, 
+          en = 11, 
+          d4 = 5, 
+          d5 = 4, 
+          d6 = 3, 
+          d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);  // กำหนด คอลัม และแถว ของหน้าจอ LCD ที่มาใช้งาน
+  lcd.print("HELLO-KANNATHAM");  // แสดงข้อความบนหน้าจอ.
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+
+}
